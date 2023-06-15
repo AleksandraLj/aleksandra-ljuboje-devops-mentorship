6ef610c7a90e5254fa59186ede946aa6d9f5cef5
Aleksandra Ljuboje <aleksandraljuboje@gmail.com>
2023-06-15 22:52:21 +0200

    Deleted files

--- task-12/ansible_playbooks/mysql-script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
- sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
- wget http://dev.mysql.com/get/mysql57-community-release-el7-8.noarch.rpm
- sudo yum localinstall -y mysql57-community-release-el7-8.noarch.rpm
- sudo yum install -y mysql-community-server
- sudo systemctl start mysqld
- sudo systemctl enable mysqld
--- task-12/packer/shell-scripts/install-nginx.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-echo "This is script to enable nginx  yum repositories"
-sleep 30
-echo Updating yum 
-sudo yum update -y
-sudo yum install -y yum-utils
-
