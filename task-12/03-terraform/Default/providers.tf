6a303890588617b9e79c2e540ecb593d8b727056
Aleksandra Ljuboje <123902808+AleksandraLj@users.noreply.github.com>
2023-06-17 11:44:37 +0200

    Update providers.tf

--- task-12/03-terraform/Default/providers.tf
+++ task-12/03-terraform/Default/providers.tf
@@ -9,7 +9,4 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region                   = "eu-central-1"
-  shared_config_files      = ["~/.aws/config"]
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "aleksIaC"
 }
